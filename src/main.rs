ac8d4d5da152a1aff58626bd3b87c04ca1233f2c
Kavita <kavi.pdri@gmail.com>
2022-09-22 18:27:50 -0700

    iterators

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
+        process::exit(0_0);
     });
 
     if let Err(e) = minigrep::run(config) {
